9c7e23e287bc6d26e60f08b9b4afbee1ee1f0d4d
girlaine.bsilva <girlaine.bsilva@CAS624988W1036.senacsp.edu.br>
2019-03-01 21:59:22 -0300

    operação i++

--- geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/GerenProd.java
+++ geren-prod/src/main/java/br/senac/tads/pi3/gerenprod/GerenProd.java
@@ -12,7 +12,8 @@ package br.senac.tads.pi3.gerenprod;
 public class GerenProd {
     public static void main(String[] args) {
         System.out.println("TEste Maven");
-        
+        int i = 0;
+        System.out.println("Valor de i++: " + i++);
     }
     
 }
