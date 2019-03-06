94d0c35cd7f1b0c6955b2424d003b22b9cb90f89
gibsilva <44347886+gibsilva@users.noreply.github.com>
2019-03-05 22:46:40 -0300

    Codigo DAO

--- geren-prod/src/main/java/DAO/ProdutoDAO.java
+++ geren-prod/src/main/java/DAO/ProdutoDAO.java
@@ -5,10 +5,48 @@
  */
 package DAO;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  *
  * @author Gi
  */
 public class ProdutoDAO {
+     private Connection obterConexao() throws ClassNotFoundException, SQLException {
+        // 1) Declarar o driver JDBC de acordo com o Banco de dados usado
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        
+        // 2) Abrir a conexão
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/produtobd?useTimezone=true&serverTimezone=UTC",
+                "root",
+                "");
+        return conn;
+    }
+    
+    public List<String> listar() {
+        String sql = "SELECT id, nome FROM CATEGORIA";
+        // CÓDIGO ABAIXO SOMENTE PARA JAVA 7 OU SUPERIOR
+        try (Connection conn = obterConexao();
+                PreparedStatement stmt = conn.prepareStatement(sql);
+                ResultSet resultados = stmt.executeQuery()) {
+            while (resultados.next()) {
+                // TRATAR RESULTADOS
+                int id = resultados.getInt("id");
+                String nome = resultados.getString("nome");
+                System.out.println("id: " + id + ", nome: " + nome);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
     
 }
