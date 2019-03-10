6022b7d85cf041510b4dd8114be3600135f6748b
adri88anne <37552570+adri88anne@users.noreply.github.com>
2019-03-10 15:41:55 -0300

    Realizada a criação do Controller

--- geren-prod/src/main/java/Controller/ProdutoController.java
+++ geren-prod/src/main/java/Controller/ProdutoController.java
@@ -1,14 +1,38 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
+
+import java.util.List;
 
 /**
  *
  * @author Gi
  */
 public class ProdutoController {
-    
+
+    public static List<Produto> procurarProduto(String nome) {
+        List<Produto> listaResposta = null;
+
+        try {
+            if (nome == null || "".equals(nome)) {
+                listaResposta = ProdutoDAO.listar();
+            } else {
+                listaResposta = ProdutoDAO.procurar(nome);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return listaResposta;
+    }
+
+    public static Produto obterProduto(Integer id) {
+        Produto produtoResposta = null;
+
+        try {
+            produtoResposta = ProdutoDAO.obter(id);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return produtoResposta;
+    }
+
 }
